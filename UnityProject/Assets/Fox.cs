3e46afc752497db519a629e3409b845c5c756347
oldliu <57282807+jimmy90709@users.noreply.github.com>
2019-11-23 11:29:36 +0800

    作業3

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(1f, 50f)]
+    public float Speed = 20;
+    public Rigidbody2D Rig;
+    public SpriteRenderer Sr;
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        Rig.AddForce(Vector2.right * Speed * x);
+        if(x >= 0)
+        {
+            Sr.flipX = false;
+        }
+        else
+        {
+            Sr.flipX = true;
+        }
+    }
+    
+}
